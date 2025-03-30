70d012e382533bc2fbc5afea711ceec52e48495b
WilliamChengpeng <cheng.peng@northeastern.edu>
2025-03-30 15:51:17 -0700

    Schedule

--- Assets/ScheduleUI.cs
+++ Assets/ScheduleUI.cs
@@ -3,16 +3,21 @@ using UnityEngine.UI;
 
 public class ScheduleUI : MonoBehaviour
 {
-    public GameObject schedulePanel;
-    public Button logoButton;
+    public GameObject schedulePanel;      
+    public GameObject scheduleTablePanel; 
+    public Button logoButton;             
+
     void Start()
     {
         logoButton.onClick.AddListener(ToggleSchedule);
         schedulePanel.SetActive(false);
+        scheduleTablePanel.SetActive(false);
+    }
 
-        void ToggleSchedule()
-        {
-            schedulePanel.SetActive(!schedulePanel.activeSelf);
-        }
+    void ToggleSchedule()
+    {
+        bool isActive = !schedulePanel.activeSelf; 
+        schedulePanel.SetActive(isActive);
+        scheduleTablePanel.SetActive(isActive);
     }
 }
